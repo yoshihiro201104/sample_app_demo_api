0e0185dee3072a5254248cc58b51c9366cbc16a0
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-03-03 11:23:41 +0000

    first_commit

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,50 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.score = Language.get_data(list_params[:body])  #この行を追加
+    tags = Vision.get_image_data(list_params[:image])
+    if @list.save
+      tags.each do |tag|
+        @list.tags.create(name: tag)
+      end
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
+end
